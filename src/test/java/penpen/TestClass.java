f182ab9aff8045a5dc6bebbd17033bde51b590cf
Kyunghun Jeon <penpen787@gmail.com>
2016-01-22 17:19:19 +0900

    #first commit #forked with one test case

---
+++ src/test/java/penpen/TestClass.java
@@ -0,0 +1,12 @@
+package penpen;
+
+import org.junit.Test;
+
+public class TestClass {
+
+    @Test
+    public void testMethod1() {
+        System.out.println("Junit Test Worked");
+    }
+    
+}
